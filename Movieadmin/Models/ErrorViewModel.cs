a48973dbac489d0b0a7e502ef792954b387de5e8
Safaa Shneweer <w0501121@campus.nscc.ca>
2026-09-16 11:51:27 -0300

    Add project files.

---
+++ Movieadmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movieadmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
